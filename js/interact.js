2146aa8c890df61090f67b51193a3cb37f116071
Matt <mfarncomb101@gmail.com>
2018-01-02 22:26:13 +0000

    new branch commit

--- js/interact.js
+++ js/interact.js
@@ -148,25 +148,54 @@ function appendDOM (temp) {
 }
 
 //on clicking on navbar, scroll to top instanntly, no animation
-for (var i = 0; i < navBar.length; i++) {
-    navBar[i].onclick = function () {
-
-        let temp = this.id;
-        if (justPressed == "") {
-            appendDOM(temp);
-        }
-        else {
-            $("#appendSpace").removeClass("position-change");
-            $("#appendSpace").addClass("position-change-back");
-            justPressed = "";
-            setTimeout(function() {
-                appendDOM(temp);
-            }, 1000)
+// for (var i = 0; i < navBar.length; i++) {
+//     navBar[i].onclick = function () {
+//         window.scrollTo(0, 0);
+//         let temp = this.id;
+//         if (justPressed == "") {
+//             appendDOM(temp);
+//         }
+//         else {
+//             $("#appendSpace").removeClass("position-change");
+//             $("#appendSpace").addClass("position-change-back");
+//             justPressed = "";
+//             setTimeout(function() {
+//                 appendDOM(temp);
+//             }, 1000)
         
-        }
+//         }
    
-    }
-}
+//     }
+// }
 // window.scrollTo(x-coord, y-coord);
 
-    
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+        // alert(this.hash);
+
+    // var nav = $('#about-me');
+    // if (nav.length) {
+    //     var contentNav = nav.offset().top;
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+    // }
+  });
+});
